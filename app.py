80296d72183941c9690e5ff39b2065db92ab66bb
zuhairfadholi30 <zuhairfadholi@gmail.com>
2025-05-22 09:09:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
-
+st.set_page_config(page_title="Hallo hallo HAYY", layout="centered")
 st.title("Hello, World!")
 st.write("Selamat datang di aplikasi Streamlit pertamaku!")
